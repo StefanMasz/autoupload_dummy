1701a589f73d411ba3c4ee60311efef9c9dbf258
valentin.despa <valentin.despa@aoe.com>
2015-04-13 09:21:07 +0200

    First version

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'dummy_ter_ext',
+    'decription' => 'Test extension for TYPO3',
+    'category' => 'example',
+    'author' => 'Valentin Despa',
+    'author_company' => 'AOE GmbH',
+    'author_email' => 'dev@aoe.com',
+    'shy' => 0,
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.2.2',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-6.2.99',
+            'php' => '5.3.0'
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => '',
+);
